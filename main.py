eb447cadc7f12ac19ae5023bae2ec3c07810404c
Aydenalv6282 <67181460+Aydenalv6282@users.noreply.github.com>
2022-10-16 17:31:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import csv
-from urllib.request import urlopen
 import yfinance as yf
 from matplotlib import pyplot as plt
 import numpy
@@ -9,7 +8,6 @@ import numpy
 suitable = []
 
 epochs = 100000  # LOOPS
-
 with open('Assets/RAW_DATA.csv', mode='r') as file:
     # reading the CSV file
     csvFile = csv.reader(file)
@@ -41,39 +39,40 @@ def gradient_descent_linear(x_vals, y_vals, m_now, b_now):
     return m, b
 
 
-def gradient_descent_sine(x_vals, y_vals, a_now, b_now, m_now, c_now):
+def gradient_descent_sine(x_vals, y_vals, a_now, b_now, c_now, m_now, d_now):
     n = len(x_vals)
     a_gradient = 0
     b_gradient = 0
-    m_gradient = 0
     c_gradient = 0
+    m_gradient = 0
+    d_gradient = 0
     a_gradient2 = 0
     b_gradient2 = 0
+    c_gradient2 = 0
     m_gradient2 = 0
-    c_gradient2 = 2/n+2
-    # Partial[Power[\(40)y-\(40)a*sin\(40)bx\(41)+mx+c\(41)\(41),2],m]
+    d_gradient2 = 2/n+2
+    # (Divide[1,n])*Sum[Power[\(40)y\(40)i\(41)-\(40)a*sin\(40)bx\(40)i\(41)+c\(41)+mx\(40)i\(41)+d\(41)\(41),2],{i,0,n}]
     for i in range(n):
         x = x_vals[i]
         y = y_vals[i]
         bx = b_now*x
-        sbx = numpy.sin(bx)
-        cbx = numpy.cos(bx)
-        a_gradient += (-2/n)*(sbx*(y-(a_now*sbx+c_now+m_now*x)))
-        a_gradient2 += (-2/n)*-(sbx**2)
-        b_gradient += (-2/n)*(a_now*cbx*x)*(y-(a_now*sbx+c_now+m_now*x))
-        b_gradient2 += (-2/n)*(a_now*(x**2))*(y*sbx-(a_now*(cbx**2)+c_now*sbx+a_now*(sbx**2)+m_now*sbx*x))
-        m_gradient += (-2/n)*(x)*(y-(a_now*sbx+c_now+m_now*x))
+        sbcx = numpy.sin(bx+c_now)
+        cbcx = numpy.cos(bx+c_now)
+        a_gradient += (-2/n)*(sbcx)*(y-(a_now*sbcx+d_now+m_now*x))
+        a_gradient2 += (2/n)*(sbcx**2)
+        b_gradient += (-2/n)*(a_now*cbcx*x)*(y-(a_now*sbcx+d_now+m_now*x))
+        b_gradient2 += (2/n)*(a_now*(x**2))*(-a_now*(sbcx**2)+a_now*(cbcx**2)-d_now*sbcx-m_now*x*sbcx+y*sbcx)
+        c_gradient += (-2/n)*(a_now*cbcx)*(y-(a_now*sbcx+d_now+m_now*x))
+        c_gradient2 += (2/n)*(a_now)*(a_now*(cbcx**2)+y*sbcx-a_now*(sbcx**2)-d_now*sbcx-m_now*x*sbcx)
+        m_gradient += (-2/n)*(x)*(y-(a_now*sbcx+d_now+m_now*x))
         m_gradient2 += (2/n)*(x**2)
-        c_gradient += (-2/n)*(y-(a_now*sbx+c_now+m_now*x))
-    a = a_now - a_gradient * abs((1/a_gradient2))
-    b = b_now - b_gradient * abs((1/b_gradient2))
-    m = m_now - m_gradient * abs((1/m_gradient2))
-    c = c_now - c_gradient * abs((1/c_gradient2))
-    line_vals_app = []
-    #for x in x_vals:
-        #line_vals_app.append(a*numpy.sin(b*x)+m*x+c)
-    #plt.plot(x_vals, line_vals_app, color="yellow")  # Gradient Descent Progress
-    return a, b, m, c
+        d_gradient += (-2/n)*(y-(a_now*sbcx+d_now+m_now*x))
+    a = a_now - a_gradient * abs(1/a_gradient2)
+    b = b_now - b_gradient * abs(1/b_gradient2)
+    c = c_now - c_gradient * abs(1/c_gradient2)
+    m = m_now - m_gradient * abs(1/m_gradient2)
+    d = d_now - d_gradient * abs(1/d_gradient2)
+    return a, b, c, m, d
 
 
 def absolute_best_line(x_vals, y_vals):  # AP STAT METHOD
@@ -106,19 +105,24 @@ for s in suitable:
     m1, b1 = 0, 0
 
     #Sine Variables
-    a, b, m, c = 0.1, 0.1, 0.1, 0.1
+    a, b, c, m, d = 0.1, 0.1, 0.1, 0.1, 0.1
     for i in range(epochs):
         m1, b1 = gradient_descent_linear(x_vals, y_vals, m1, b1)
-        a, b, m, c = gradient_descent_sine(x_vals, y_vals, a, b, m, c)
+        a, b, c, m, d = gradient_descent_sine(x_vals, y_vals, a, b, c, m, d)
     m2, b2 = absolute_best_line(x_vals, y_vals)
     line_vals = []
+    sine_x_vals = []
     sine_vals = []
+    sine_smooth_vals = []
     for x in x_vals:
         line_vals.append(m1*x+b1)
-        sine_vals.append(a * numpy.sin(b * x) + m * x + c)
-    resl = 
+        sine_vals.append(a * numpy.sin(b * x + c) + m * x + d)
+    for i in range(len(x_vals)*100):
+        x = i/100
+        sine_smooth_vals.append(a * numpy.sin(b * x + c) + m * x + d)
+        sine_x_vals.append(x)
     plt.plot(x_vals, line_vals, color="red")  # Linear Gradient Descent
-    print(sum(numpy.square(numpy.subtract(y_vals, line_vals))))
-    plt.plot(x_vals, sine_vals, color="purple")  # Sine Gradient Descent
-    print(sum(numpy.square(numpy.subtract(y_vals, sine_vals))))
+    plt.plot(sine_x_vals, sine_smooth_vals, color="purple")  # Sine Gradient Descent
+    print(sum(numpy.subtract(y_vals, sine_vals)), sum(numpy.square(numpy.subtract(y_vals, sine_vals))))
+    print(a, b, c, m, d)
 plt.show()
